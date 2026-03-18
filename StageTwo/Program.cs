d6ae201160844a435a093e55d4f047219ada9515
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-03-18 11:55:55 +0700

    done day 2

--- StageTwo/Program.cs
+++ StageTwo/Program.cs
@@ -1,13 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+// mặc định tìm kiếm file index.html, index.htm, default.html, default.html
+app.UseDefaultFiles();
+
+// dùng các file tĩnh css, js, image,... trong wwwroot
 app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}"
+    pattern: "{controller=Account}/{action=Index}"
 );
 
 app.Run();
---
+++ StageTwo/wwwroot/js/main.js
@@ -0,0 +1,6 @@
+﻿let pic = document.getElementById("avatar");
+pic.addEventListener("click", () => {
+    pic.style.cursor = "pointer";
+    pic.style.filter = "blur(1px)";
+    pic.style.color = "blue";
+});
