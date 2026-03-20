8998d6c3f2dad00af4847dc60e6396e41c0478b0
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-03-20 10:37:38 +0700

    Initial commit stage 3

---
+++ StageThree/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StageThree.Controllers;
+
+public class ProductController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
