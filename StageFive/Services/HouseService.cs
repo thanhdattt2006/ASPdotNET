4c0480206466c61341d62c1b760d0da2b65a832b
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-03-30 12:02:45 +0700

    done stage five

---
+++ StageFive/Services/HouseService.cs
@@ -0,0 +1,7 @@
+﻿namespace StageFive.Services;
+
+public class HouseService : IHouseService
+{
+    public double Area(double length, double width) => CalcService.Multiply(width , length);
+    public double Perimeter(double length, double width) => CalcService.Sum(width,length) *  2;
+}
---
+++ StageFive/Services/IHouseService.cs
@@ -0,0 +1,7 @@
+﻿namespace StageFive.Services;
+
+public interface IHouseService
+{
+    double Area(double length, double width);
+     double Perimeter(double length, double width);
+}
