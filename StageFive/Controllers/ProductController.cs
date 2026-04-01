44ffcdd7be7f7e967e5607e31d31c4b6d68c59bc
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-04-01 11:08:21 +0700

    return view with fake data

---
+++ StageFive/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StageFive.Services;
+
+namespace StageFive.Controllers;
+
+[Route("products")]
+public class ProductController (IProductService productService) : Controller
+{
+    [Route("")]
+    [Route("index")]
+    public IActionResult Index()
+    {
+        ViewBag.AllProducts = productService.GetProducts();
+        return View();
+    }
+
+    [Route("index1")]
+    public IActionResult Index1() => View(productService.GetProducts() ?? null);
+
+    [Route("index2/{id}")]
+    public IActionResult Index2(int id) => View(productService.GetProductById (id) ?? null);
+}
