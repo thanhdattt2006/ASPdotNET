da344a8d0d4abcba2d2e98a7a41d4af1ac9cda52
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-04-06 10:45:36 +0700

    httpget, form get in .net

---
+++ StageSix/Controllers/FormController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StageSix.Controllers;
+
+[Route("Form")]
+public class FormController : Controller
+{
+    [Route("index")]
+    [Route("")]
+    [HttpGet("~/")]
+    public IActionResult Index() => View();
+
+    [HttpGet("show-name")]
+    public IActionResult ShowName(string fullname) => View(model: fullname);
+}
