6b809fc426e169d07386e6d007a8377d9e59da8d
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-04-24 23:42:30 +0700

    ajax post

--- StageSeven/Controllers/AjaxApiController.cs
+++ StageSeven/Controllers/AjaxApiController.cs
@@ -10,7 +10,16 @@ public class AjaxApiController : Controller
     [HttpGet("~/")]
     public IActionResult Index() => View();
 
-    [HttpGet("message-json-async")]
+    [HttpGet("message-json-async", Name = "MessageJsonAsync")]
     public async Task<IActionResult> MessageJsonAsync()
     => await Task.FromResult(Json(new { message = "Ajax: Hello World Json Async!" }));
+
+    [HttpGet("get-name-query", Name = "GetNameQuery")]
+    public IActionResult GetNameQuery(string name) => Json(new { fullname = $"hello, {name}: " });
+
+    [HttpPost("get-person", Name = "GetPerson")]
+    public IActionResult GetPerson([FromBody] PersonVM person)
+        => person is null
+        ? BadRequest("Person data is required")
+        : !ModelState.IsValid ? BadRequest(ModelState) : Ok(new { fullname = person.Name, age = person.Age });
 }
---
+++ StageSeven/wwwroot/js/ajax-api/message-json-async.js
@@ -0,0 +1,22 @@
+﻿import apiService from '/js/shared/apiService.js';
+document.getElementById('btnMessageJsonAsync').addEventListener('click',
+	async () => {
+		try {
+			// cách 1 nhanh
+			//const url1 = '/ajaxapi/message-json-async'
+
+			// cách 2 phức tạp
+			//const url2 = `@Url.Action("message-json-async", "Ajaxapi")`;
+
+			// cách 3 mạnh nhất, dễ bảo trì
+			//const url3 = `@Url.RouteUrl("MessageJsonAsync")`
+
+			const url3 = document.getElementById('btnMessageJsonAsync').getAttribute('data-url');
+			//const url3 = docutment.getElementById('btnMessageJsonAsync').dataset.url; ko phổ biến bằng getAttribute
+			const result = await apiService.get(url3);
+			document.getElementById('resultJsonAsync').textContent = result.message;
+		} catch (error) {
+			console.error('error fetch message: ', error);
+			document.getElementById('resultJsonAsync').textContent = 'Error fetching message';
+		}
+	});
