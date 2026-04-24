c120357b6f36881fd9e03ad77992015348a803c2
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-04-24 10:39:31 +0700

    advanced ajax apiService.js

---
+++ StageSeven/Controllers/AjaxApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StageSeven.Controllers;
+
+[Route("ajaxapi")]
+public class AjaxApiController : Controller
+{
+    [Route("")]
+    [Route("index")]
+    [HttpGet("~/")]
+    public IActionResult Index() => View();
+
+    [HttpGet("message-json-async")]
+    public async Task<IActionResult> MessageJsonAsync()
+    => await Task.FromResult(Json(new { message = "Ajax: Hello World Json Async!" }));
+}
